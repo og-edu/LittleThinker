5ba09426ac7d0a441e85ce829c0cd40c927234e6
nova Custom <nova@custom.fr>
2025-06-18 14:45:03 +0200

    TODO.java : mis A jour

--- fr/umlv/littlethinker/main/TODO.java
+++ fr/umlv/littlethinker/main/TODO.java
@@ -3,10 +3,10 @@ package fr.umlv.littlethinker.main;
 public class TODO {
     // DONE : afficher les adresses en hexadEcimal
     // TODO : mnEmonique ld (et autres) compatibles avec l'hexadecimal
+    // DONE : ramVideo : rEserver une place en mEmoire haute pour la pile
     // TODO :
     // TODO :
-    // TODO :
-    // TODO : cases mEmoires communes A ravm vidEo : changer la couleur de fond
+    // TODO : cases mEmoires communes A ram vidEo : changer la couleur de fond
     // TODO : octets de la colonne gauche de la ram video : couleur diffErente
     // TODO : indiquer dans l'interface la taille de la mémoire vidEo
     // TODO : brider les valeurs maxi saisissables à 255 (dans toutes les bases)
@@ -17,7 +17,7 @@ public class TODO {
 
     // TODO : POUVOIR CONFIGURER (via fichier de ressources et redémarrage)
     // TODO : aggrandir la hauteur de la zone 'REGISTER'
-    // TODO : la taille de la mémoire vidéo  enum 1x3 / 1x8 / 2x3 / 2x8 /3x8
+    // DONE : la taille de la mémoire vidéo  enum 1x3 / 1x8 / 2x3 / 2x8 /3x8
     // TODO : config pour choisir l'affichage des adresses hexa ou dec
     // TODO :
     // TODO :
